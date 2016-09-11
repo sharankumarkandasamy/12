2061e4b5a5c4541f18873ad3a0369c6033e9d380
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-09-11 14:50:56 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class reversestring 
+{
+	 public static void main(String args[])
+	   {
+	      String original, reverse = "";
+	      Scanner in = new Scanner(System.in);
+	 
+	      System.out.println("Enter a string to reverse");
+	      original = in.nextLine();
+	 
+	      int length = original.length();
+	 
+	      for ( int i = length - 1 ; i >= 0 ; i-- )
+	         reverse = reverse + original.charAt(i);
+	 
+	      System.out.println("string is: "+reverse);
+	   }
+}
